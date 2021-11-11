a1d17f4077585af81235b1af6c3c3216f4ba9ddc
N_egor <egordeniko@gmail.com>
2021-11-11 11:33:29 +0300

    dfsf

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,9 +1,6 @@
 <template>
-<<<<<<< HEAD
   <!-- eslint-disable -->
 
-=======
->>>>>>> second
   <div class="post">
     <div>
       <div>
@@ -13,47 +10,25 @@
         Description: <strong> {{ post.body }} </strong>
       </div>
     </div>
-<<<<<<< HEAD
     <div class="post__btns">
       <MyButton
       @click="$emit('remove', post)">Delete</MyButton>
     </div>
-=======
-    <MyButton @click="removePost"> Remove </MyButton>
->>>>>>> second
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import MyButton from "../components/UI/MyButton.vue"
 export default {
     components : {
         MyButton,
     },
-=======
-import MyButton from './UI/MyButton.vue';
-export default {
-  components: { MyButton },
-  data() {
-    return {};
-  },
->>>>>>> second
   props: {
     post: {
       type: Object,
       required: true,
     },
   },
-<<<<<<< HEAD
-=======
-  methods : {
-    removePost() {
-      this.$emit('remove', this.post);
-      console.log("remove in item");
-    }
-  }
->>>>>>> second
 };
 </script>
 
@@ -66,9 +41,5 @@ export default {
   align-items: center;
   justify-content: space-between;
 }
-<<<<<<< HEAD
 
 </style>
-=======
-</style>
->>>>>>> second
